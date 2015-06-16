1f858c29b92f4be29ef55d79f64335e9e76b3aab
Romain <romaincourtieux@hotmail.com>
2015-06-17 00:12:31 +0200

    maj

--- GoPizza/src/com/efrei/gopizza/Payment.java
+++ GoPizza/src/com/efrei/gopizza/Payment.java
@@ -1,11 +1,17 @@
 package com.efrei.gopizza;
 
 import com.example.gopizza.R;
-
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class Payment extends Activity {
 	
@@ -17,10 +23,33 @@ public class Payment extends Activity {
 
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.order_fullscreen);
+		setContentView(R.layout.payment_fullscreen);
+
+		generateQRCode("https://github.com/n3ocat/efreiProjects/tree/master/GoPizza");
 
 	}
-	
+
+	public void generateQRCode(String url) {
+
+		QRCodeWriter writer = new QRCodeWriter();
+	    try {
+	        BitMatrix bitMatrix = writer.encode(url, BarcodeFormat.QR_CODE, 512, 512);
+	        int width = bitMatrix.getWidth();
+	        int height = bitMatrix.getHeight();
+	        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+	        for (int x = 0; x < width; x++) {
+	            for (int y = 0; y < height; y++) {
+	                bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+	            }
+	        }
+	        ((ImageView) findViewById(R.id.qrcode)).setImageBitmap(bmp);
+
+	    } catch (WriterException e) {
+	        e.printStackTrace();
+	    }
+
+	}
+
 	@Override
 	public void onBackPressed() {
 
